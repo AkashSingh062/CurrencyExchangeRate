436f9aaf54d67a911f566b10c05da3bf9e879077
Akash Singh <akash622003singh@gmail.com>
2025-05-06 11:13:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 const url = "https://api.exchangerate-api.com/v4/latest/USD";
 const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector(".btn")
 
-for(let select of dropdowns) {
-  for(let currency_code in countryList) {
+const updateExchangeRate = async () => {
+  let amount = document.querySelector("input");
+  let amtVal = amount.value;
+  if (amtVal <= 0) {
+    amount.value = "1";
+    amtVal = 1;
+    console.log("object")
+  }
+  const url = `https://api.exchangerate-api.com/v4/latest/${dropdowns[0].value}`;
+
+  const rates = await fetch(url);
+  const data = await rates.json();
+  amount.value = (amtVal * data.rates[dropdowns[1].value]);
+  document.querySelector(".msg").innerText = `1 ${dropdowns[0].value} = ${data.rates[dropdowns[1].value]} ${dropdowns[1].value}`;
+  document.querySelector(".msg").style.display = "block";
+};
+const btn = document.querySelector(".btn");
+
+for (let select of dropdowns) {
+  for (let currency_code in countryList) {
     let option = document.createElement("option");
     option.value = currency_code;
     option.innerText = currency_code;
-    if(select.name == "from" && currency_code == "USD") {
+    if (select.name == "from" && currency_code == "USD") {
       option.selected = true;
     }
-    if(select.name == "to" && currency_code == "INR") {
+    if (select.name == "to" && currency_code == "INR") {
       option.selected = true;
     }
     select.appendChild(option);
   }
-  select.addEventListener("change", (evt)=>{
+  select.addEventListener("change", (evt) => {
     updateFlag(evt.target);
   });
 }
@@ -24,19 +41,11 @@ const updateFlag = (ele) => {
   let img = ele.parentElement.querySelector("img").src = `https://flagsapi.com/${countryCode}/shiny/48.png`;
 }
 btn.addEventListener("click", async (evt) => {
-    evt.preventDefault();
-    let amount = document.querySelector("input");
-    let amtVal= amount.value;
-    if (amtVal <= 0) {
-        amount.value = "1";
-        amtVal= 1;
-        console.log("object")
-    }
-    const url = `https://api.exchangerate-api.com/v4/latest/${dropdowns[0].value}`;
+  evt.preventDefault();
+  updateExchangeRate();
 
-    const rates = await fetch(url);
-    const data = await rates.json();
-    amount.value = (amtVal * data.rates[dropdowns[1].value]);
-    document.querySelector(".msg").innerText = `1 ${dropdowns[0].value} = ${data.rates[dropdowns[1].value]} ${dropdowns[1].value}`;
-    document.querySelector(".msg").style.display = "block";
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  updateExchangeRate();
 });
